794d4b62edd4b46b46a0678676e9fe523cac324b
Ashwani Kumar <akumar@monmouthcollege.edu>
2018-06-21 15:29:03 -0500

    handle Empty or null cases

--- app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
+++ app/src/main/java/com/example/android/didyoufeelit/MainActivity.java
@@ -73,8 +73,12 @@ public class MainActivity extends AppCompatActivity {
          */
         @Override
         protected Event doInBackground(String... urls) {
+            // Don't perform the request if there are no URLs, or the first URL is null.
+            if(urls.length < 1 || urls[0] == null){
+                return null;
+            }
 
-            // Perform the HTTP request for earthquake data and process the response.
+            // Perform the HTTP request for earthquake data  in the background
             Event result = Utils.fetchEarthquakeData(urls[0]);
             return result;
         }
@@ -88,6 +92,10 @@ public class MainActivity extends AppCompatActivity {
          */
         @Override
         protected void onPostExecute(Event result) {
+            // if there is no result, do nothing.
+            if (result == null) {
+                return;
+            }
             updateUi(result);
         }
     }
